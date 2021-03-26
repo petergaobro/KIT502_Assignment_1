b9438aac9d9cc0901859140a940ccc516aa9366a
Whourdaddy <ganmengqi@gmail.com>
2021-03-26 19:10:09 +1100

    change

--- js/password-admin.js
+++ js/password-admin.js
@@ -69,64 +69,120 @@ admin_log_form.addEventListener('submit', (e) => {
     else if(!sys_pws_format.test(login_admin_sys_pws.value)){
         alert('Password must contain at least 1 lower case letter,1 uppercase letter,1 number and the following special charaters(!@#$%)');
     }
-        // isvalid= true;
-    
-    // if (login_admin_pws.value.length >= 20) {
-    //     confirm('Password must be less than 20 characters');
-    // }
     else if(sys_pws_format.test(login_admin_sys_email.value) && sys_email_format.test(login_admin_sys_email.value)) {
         alert('Login in successfull.');
         window.location.href="../html/dashboard.html";
     }
     return isvalid;
 })
-
+function pws_macth() {
+    var reg_sys_pws = document.getElementById("reg_sys_pws");
+    var reg_sys_conpws = document.getElementById("reg_sys_conpws");
+    if (reg_sys_pws.value != reg_sys_conpws.value) {
+        alert("passwprd not match");
+    } else if (ureg_sys_pws.value != "" || reg_sys_conpws.value != "") {
+        alert("please enterlonger than 6 characters and Password must be less than 12 characters.Also it must contain at least 1 lower case letter,1 uppercase letter,1 number and the following special charaters(!@#$%) ")
+    } else {
+        location.href ="../html/login_admin.html"
+    }
+}
+function host_pws_macth() {
+    var reg_host_pws = document.getElementById("reg_host_pws");
+    var reg_host_confpws = document.getElementById("reg_host_confpws");
+    if (reg_host_pws.value != reg_host_confpws.value) {
+        alert("passwprd not match");
+    } else if (reg_host_pws.value != "" || reg_host_confpws.value != "") {
+        alert("please enterlonger than 6 characters and Password must be less than 12 characters.Also it must contain at least 1 lower case letter,1 uppercase letter,1 number and the following special charaters(!@#$%) ")
+    } else {
+        location.href ="../html/login_admin.html"
+    }
+}
 
 
 
 
 // ----------------host register---------------------
-const reg_sys_fname = document.getElementById('reg_sys_fname')
-const reg_sys_lname = document.getElementById('reg_sys_lname')
-const reg_sys_email = document.getElementById('reg_sys_email')
-const reg_sys_pws = document.getElementById('reg_sys_pws')
-const reg_sys_conpws = document.getElementById('reg_sys_conpws')
-const reg_sys_pws_format = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%])[A-Za-z\d!@#$%]{6,12}$/
-const reg_sys_email_format = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-// const isvalid = false
-reg_form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    // let messages = []
-    if (reg_sys_email.value == '' || reg_sys_email.value == null) {
-        alert('Name is required');
-        // isvalid = true;
-    }
-    else if(!reg_sys_email_format.test(reg_sys_email.value)){
-        alert('Invalid email format');
-    }
-    else if (reg_host_pws.value.length <= 5 || reg_host_pws.value.length >= 13) {
-        alert('Password must be longer than 6 characters and Password must be less than 12 characters');
-        // isvalid = true;
-    }
-    else if(!reg_sys_pws_format.test(reg_sys_pws.value)&&(reg_sys_conpws.value)){
-        alert('Password must contain at least 1 lower case letter,1 uppercase letter,1 number and the following special charaters(!@#$%)');
-    }
+// const reg_host_fname = document.getElementById('reg_host_fname')
+// const reg_host_lname = document.getElementById('reg_host_lname')
+// const reg_host_email = document.getElementById('reg_host_email')
+// const reg_host_pws = document.getElementById('reg_host_pws')
+// const reg_host_confpws = document.getElementById('reg_host_confpws')
+// const reg_host_abn = document.getElementById('reg_host_abn');
+// const reg_host_pws_format = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%])[A-Za-z\d!@#$%]{6,12}$/
+// const reg_hostemail_format = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+// // const isvalid = false
+// reg_admin_form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     // let messages = []
+//     if (reg_host_email.value == '' || reg_host_email.value == null) {
+//         alert('Email is required');
+//         // isvalid = true;
+//     }
+//     else if(!reg_host_email.test(reg_host_email.value)){
+//         alert('Invalid email format');
+//     }
+//     else if (reg_host_pws.value.length <= 5 || reg_host_pws.value.length >= 13) {
+//         alert('Password must be longer than 6 characters and Password must be less than 12 characters');
+//         // isvalid = true;
+//     }
+//     else if(!reg_host_pws_format.test(reg_host_pws.value)&&(reg_host_confpws.value)){
+//         alert('Password must contain at least 1 lower case letter,1 uppercase letter,1 number and the following special charaters(!@#$%)');
+//     }
 
-    // if (login_password.value.length >= 20) {
-    //     confirm('Password must be less than 20 characters');
-    // }
-    else if (reg_sys_pws.value!=reg_sys_conpws.value) {
-        alert('Password does not match');
-    }
-    else if(reg_sys_pws.value ==reg_sys_conpws.value) {
-        alert('Sign up successfull.Please sign in');
-        window.location.href="../html/login_admin.html";
-    }
-    return isvalid;
-})
+//     // if (login_password.value.length >= 20) {
+//     //     confirm('Password must be less than 20 characters');
+//     // }
+//     else if (reg_host_pws.value!=reg_host_confpws.value) {
+//         alert('Password does not match');
+//     }
+//     else if(reg_host_pws.value ==reg_host_pws.value) {
+//         alert('Sign up successfull.Please sign in');
+//         window.location.href="../html/login_admin.html";
+//     }
+//     return isvalid;
+// })
 
 
-// ---------systemer regiter------
+// // ---------systemer regiter------
+
+// const reg_sys_fname = document.getElementById('reg_sys_fname')
+// const reg_sys_lname = document.getElementById('reg_sys_lname')
+// const reg_sys_email = document.getElementById('reg_sys_email')
+// const reg_sys_pws = document.getElementById('reg_sys_pws')
+// const reg_sys_conpws = document.getElementById('reg_sys_conpws')
+// const reg_sys_pws_format = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%])[A-Za-z\d!@#$%]{6,12}$/
+// const reg_sys_email_format = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+// // const isvalid = false
+// reg_admin_form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     // let messages = []
+//     if (reg_sys_email.value == '' || reg_sys_email.value == null) {
+//         alert('Name is required');
+//         // isvalid = true;
+//     }
+//     else if(!reg_sys_email_format.test(reg_sys_email.value)){
+//         alert('Invalid email format');
+//     }
+//     else if (reg_host_pws.value.length <= 5 || reg_host_pws.value.length >= 13) {
+//         alert('Password must be longer than 6 characters and Password must be less than 12 characters');
+//         // isvalid = true;
+//     }
+//     else if(!reg_sys_pws_format.test(reg_sys_pws.value)&&(reg_sys_conpws.value)){
+//         alert('Password must contain at least 1 lower case letter,1 uppercase letter,1 number and the following special charaters(!@#$%)');
+//     }
+
+//     // if (login_password.value.length >= 20) {
+//     //     confirm('Password must be less than 20 characters');
+//     // }
+//     else if (reg_sys_pws.value!=reg_sys_conpws.value) {
+//         alert('Password does not match');
+//     }
+//     else if(reg_sys_pws.value ==reg_sys_conpws.value) {
+//         alert('Sign up successfull.Please sign in');
+//         window.location.href="../html/login_admin.html";
+//     }
+//     return isvalid;
+// })
 
 
 
